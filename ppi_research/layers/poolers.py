fixed bug in weighted average pooler and changed the prott5 sequence preprocessing to do single functionality and changed the output of split_string_sequences_to_list to be consistent if the seqence contain single or multi chain"

ppi_research/layers/poolers.py
@@ -2,13 +2,11 @@ from __future__ import annotations
 
 import torch
 from torch import nn
-from ppi_research.layers.attention import softmax as softmax_fn
 
 
 def global_mean_pooling1d(
     x: torch.FloatTensor,
     padding_mask: torch.FloatTensor | None = None,
-    dim: int = 1,
 ) -> torch.FloatTensor:
     """
     Global Mean Pooling 1D.
@@ -22,17 +20,16 @@ def global_mean_pooling1d(
         The global mean pooled tensor.
     """
     if padding_mask is None:
-        return torch.mean(x, dim=dim)
-    padding_mask = padding_mask.to(device=x.device, dtype=torch.long)
+        return torch.mean(x, dim=1)
+    padding_mask = padding_mask.to(device=x.device, dtype=x.dtype)
     padding_mask = padding_mask.unsqueeze(-1)
     x_masked = x * padding_mask
-    return x_masked.sum(dim=dim) / padding_mask.sum(dim=dim)
+    return x_masked.sum(dim=1) / padding_mask.sum(dim=1)
 
 
 def global_max_pooling1d(
     x: torch.FloatTensor,
     padding_mask: torch.FloatTensor | None = None,
-    dim: int = 1,
 ):
     """
     Global Max Pooling 1D.
@@ -46,12 +43,12 @@ def global_max_pooling1d(
         The global max pooled tensor.
     """
     if padding_mask is None:
-        return torch.amax(x, dim=dim)
+        return torch.amax(x, dim=1)
 
     padding_mask = padding_mask.to(device=x.device, dtype=torch.long)
     padding_mask = padding_mask.unsqueeze(-1)
     x = x.masked_fill(padding_mask.logical_not(), -torch.inf)
-    return torch.amax(x, dim=dim)
+    return torch.amax(x, dim=1)
 
 
 class GlobalAvgPooling1D(nn.Module):
@@ -65,9 +62,8 @@ class GlobalAvgPooling1D(nn.Module):
         self,
         x: torch.FloatTensor,
         padding_mask: torch.FloatTensor | None = None,
-        dim: int = 1,
     ):
-        return global_mean_pooling1d(x=x, padding_mask=padding_mask, dim=dim)
+        return global_mean_pooling1d(x=x, padding_mask=padding_mask)
 
 
 class GlobalMaxPooling1D(nn.Module):
@@ -81,16 +77,14 @@ class GlobalMaxPooling1D(nn.Module):
         self,
         x: torch.FloatTensor,
         padding_mask: torch.FloatTensor | None = None,
-        dim: int = 1,
     ):
-        return global_max_pooling1d(x=x, padding_mask=padding_mask, dim=dim)
+        return global_max_pooling1d(x=x, padding_mask=padding_mask)
 
 
 class AttentionPooling1D(nn.Module):
     def __init__(
         self,
         embed_dim: int,
-        add_one_to_softmax: bool = False,
         bias: bool = False,
     ):
         """
@@ -98,32 +92,24 @@ class AttentionPooling1D(nn.Module):
 
         Args:
             embed_dim: The dimension of the embeddings.
-            add_one_to_softmax: Whether to add one to the softmax denominator.
             bias: Whether to use a bias in the linear layer.
         """
         super().__init__()
-        self.add_one_to_softmax = add_one_to_softmax
         self.w_proj = nn.Linear(embed_dim, 1, bias=bias)
 
     def forward(
         self,
         x: torch.FloatTensor,
         padding_mask: torch.FloatTensor | None = None,
-        dim: int = 1,
     ):
         outputs = self.w_proj(x).squeeze(-1)
 
         if padding_mask is not None:
-            padding_mask = padding_mask.to(device=x.device, dtype=torch.long)
-            outputs = outputs.masked_fill(
-                padding_mask.logical_not(), -torch.inf
-            )
+            padding_mask = padding_mask.to(device=x.device, dtype=x.dtype)
+            outputs = outputs + padding_mask.log()
 
-        if self.add_one_to_softmax:
-            probs = softmax_fn(outputs, dim=-1)
-        else:
-            probs = nn.functional.softmax(outputs, dim=-1)
-        return torch.sum(x * probs.unsqueeze(-1), dim=dim)
+        probs = nn.functional.softmax(outputs, dim=-1)
+        return torch.sum(x * probs.unsqueeze(-1), dim=1)
 
 
 class WeightedAveragePooling1D(nn.Module):
@@ -142,87 +128,19 @@ class WeightedAveragePooling1D(nn.Module):
         self,
         x: torch.FloatTensor,
         padding_mask: torch.FloatTensor | None = None,
-        dim: int = 1,
     ):
         outputs = self.w_proj(x).squeeze(-1)
+        if padding_mask is None:
+            gates = nn.functional.sigmoid(outputs)
+            return torch.mean(x * gates.unsqueeze(-1), dim=1)
 
-        if padding_mask is not None:
-            padding_mask = padding_mask.to(device=x.device, dtype=torch.long)
-            outputs = outputs.masked_fill(
-                padding_mask.logical_not(), -torch.inf
-            )
-
+        padding_mask = padding_mask.to(device=x.device, dtype=x.dtype)
+        outputs = outputs + padding_mask.log()
         gates = nn.functional.sigmoid(outputs)
-        return torch.mean(x * gates.unsqueeze(-1), dim=dim)
+        padding_mask = padding_mask.unsqueeze(-1)
+        return (x * gates.unsqueeze(-1)).sum(dim=1) / padding_mask.sum(dim=1)
 
 
-class ChainsPoolerV2(nn.Module):
-    def __init__(self, pooler: nn.Module):
-        super().__init__()
-        self.pooler = pooler
-
-    def forward(
-        self,
-        protein_embed: torch.FloatTensor,
-        chain_ids: torch.LongTensor,
-    ) -> torch.FloatTensor:
-        # Vectorized implementation
-        unique_chains = torch.unique(chain_ids)
-
-        # Create a mask of shape [num_unique_chains, batch_size]
-        masks = torch.stack(
-            [
-                (chain_ids == chain_id)
-                for chain_id in unique_chains
-            ],
-        )
-
-        # Expand protein_embed for broadcasting
-        expanded_embed = protein_embed.unsqueeze(0).expand(
-            len(unique_chains),
-            -1,
-            -1,
-        )
-
-        # Apply masks and pool
-        # masked_embeds = expanded_embed * masks.unsqueeze(-1)
-
-        pooled_chains = self.pooler(
-            expanded_embed,
-            masks.to(dtype=torch.long),
-            dim=1,
-        )
-        return pooled_chains
-
-
-class ChainsPooler(nn.Module):
-    def __init__(self, pooler: nn.Module):
-        super().__init__()
-        self.pooler = pooler
-
-    def forward(
-        self,
-        protein_embed: torch.FloatTensor,
-        chain_ids: torch.LongTensor,
-    ) -> torch.FloatTensor:
-        outputs = []
-        for chain_id in torch.unique(chain_ids):
-            mask = chain_ids == chain_id
-            protein_embed_masked = protein_embed[mask, ...]
-            pooled_chains = self.pooler(
-                protein_embed_masked, dim=0,
-            )
-
-            # .unsqueeze(0) to add a batch dimension.
-            outputs.append(pooled_chains.unsqueeze(0))
-        return torch.cat(outputs, dim=0)
-
-
-# Chains Poolers should not be
-# included here, given that they are not
-# technically poolers, they are a
-# container that aggregate
-# chains and pools them with an already existing pooler.
 available_poolers = {
     "avg": GlobalAvgPooling1D,
     "max": GlobalMaxPooling1D,

ppi_research/models/convbert/multichain.py
@@ -18,6 +18,12 @@ def aggregate_chains(sequence_1, sequence_2, aggregation_method: str):
         raise ValueError(f"Invalid aggregation method: {aggregation_method}")
 
 
+def report(x, y):
+    allc = torch.allclose(x, y)
+    allt = torch.all(x == y).item()
+    return allc, allt
+
+
 class MultiChainConvBERTModel(nn.Module):
     def __init__(
         self,
@@ -141,14 +147,7 @@ class MultiChainConvBERTModel(nn.Module):
             -1,
         )
 
-        # Apply masks and pool
-        # masked_embeds = expanded_embed * masks.unsqueeze(-1)
-
-        pooled_chains = pooler(
-            expanded_embed,
-            masks.to(dtype=torch.long),
-            dim=1,
-        )
+        pooled_chains = pooler(expanded_embed, masks)
         return pooled_chains
 
     def process_chains(
@@ -157,7 +156,7 @@ class MultiChainConvBERTModel(nn.Module):
         chain_ids: torch.LongTensor,
         pooler: nn.Module,
     ) -> torch.FloatTensor:
-        """DEPRECATED: Non-vectorized implementation of the chains pooling.
+        """Non-vectorized implementation of the chains pooling.
 
         Args:
             protein_embed (torch.FloatTensor): The protein embeddings.
@@ -168,16 +167,10 @@ class MultiChainConvBERTModel(nn.Module):
             torch.FloatTensor: The pooled chains.
         """
         outputs = []
-        for chain_id in torch.unique(chain_ids):
+        unique_chain_ids = torch.unique(chain_ids)
+        for chain_id in unique_chain_ids:
             mask = chain_ids == chain_id
-            protein_embed_masked = protein_embed[mask, ...]
-            pooled_chains = pooler(
-                protein_embed_masked,
-                dim=0,
-            )
-
-            # .unsqueeze(0) to add a batch dimension.
-            outputs.append(pooled_chains.unsqueeze(0))
+            outputs.append(pooler(protein_embed[None, mask, ...]))
         return torch.cat(outputs, dim=0)
 
     def compute_loss(
@@ -261,13 +254,11 @@ class MultiChainConvBERTModel(nn.Module):
         ligand_pooled = ligand_pooler(
             ligand_embed,
             ligand_attention_mask,
-            dim=1,
         )
 
         receptor_pooled = receptor_pooler(
             receptor_embed,
             receptor_attention_mask,
-            dim=1,
         )
 
         # Process the chains
@@ -278,9 +269,9 @@ class MultiChainConvBERTModel(nn.Module):
                 else self.ligand_chains_pooler
             )
             ligand_pooled_chains = self.process_chains_v2(
-                protein_embed=ligand_pooled,
-                chain_ids=ligand_chain_ids,
-                pooler=ligand_chains_pooler,
+                ligand_pooled,
+                ligand_chain_ids,
+                ligand_chains_pooler,
             )
         else:
             ligand_pooled_chains = ligand_pooled
@@ -292,9 +283,9 @@ class MultiChainConvBERTModel(nn.Module):
                 else self.receptor_chains_pooler
             )
             receptor_pooled_chains = self.process_chains_v2(
-                protein_embed=receptor_pooled,
-                chain_ids=receptor_chain_ids,
-                pooler=receptor_chains_pooler,
+                receptor_pooled,
+                receptor_chain_ids,
+                receptor_chains_pooler,
             )
         else:
             receptor_pooled_chains = receptor_pooled

ppi_research/models/multichain.py
@@ -21,19 +21,22 @@ class MultiChainModel(nn.Module):
     def __init__(
         self,
         backbone: nn.Module,
-        ligand_global_pooler: nn.Module | str,
-        receptor_global_pooler: nn.Module | str,
-        ligand_chains_pooler: nn.Module | str,
-        receptor_chains_pooler: nn.Module | str,
-        model_name: str | None = None,
-        embedding_name: str | None = None,
+        global_pooler: nn.Module | str,
+        chains_pooler: nn.Module | str,
+        shared_global_pooler: bool = False,
+        shared_chains_pooler: bool = False,
         aggregation_method: str = "concat",
         use_ffn: bool = False,
         bias: bool = False,
+        model_name: str | None = None,
+        embedding_name: str | None = None,
     ):
         super().__init__()
+
         self.embed_dim = backbone.config.hidden_size
         self.use_ffn = use_ffn
+        self.shared_global_pooler = shared_global_pooler
+        self.shared_chains_pooler = shared_chains_pooler
         self.aggregation_method = aggregation_method
         input_dim = (
             self.embed_dim * 2
@@ -41,18 +44,29 @@ class MultiChainModel(nn.Module):
             else self.embed_dim
         )
 
-        self.ligand_global_pooler = poolers.get(
-            ligand_global_pooler, self.embed_dim
-        )
-        self.receptor_global_pooler = poolers.get(
-            receptor_global_pooler, self.embed_dim
-        )
-        self.ligand_chains_pooler = poolers.get(
-            ligand_chains_pooler, self.embed_dim
-        )
-        self.receptor_chains_pooler = poolers.get(
-            receptor_chains_pooler, self.embed_dim
-        )
+        if self.shared_global_pooler:
+            self.global_pooler = poolers.get(
+                global_pooler, self.embed_dim
+            )
+        else:
+            self.ligand_global_pooler = poolers.get(
+                global_pooler, self.embed_dim
+            )
+            self.receptor_global_pooler = poolers.get(
+                global_pooler, self.embed_dim
+            )
+
+        if self.shared_chains_pooler:
+            self.chains_pooler = poolers.get(
+                chains_pooler, self.embed_dim
+            )
+        else:
+            self.ligand_chains_pooler = poolers.get(
+                chains_pooler, self.embed_dim
+            )
+            self.receptor_chains_pooler = poolers.get(
+                chains_pooler, self.embed_dim
+            )
 
         self.backbone = BackbonePairEmbeddingExtraction(
             backbone=backbone,