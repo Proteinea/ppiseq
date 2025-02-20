9196768a5d38d3b0df73808c2a154e0cfb9a3c30
Hazem <hazemessam199@gmail.com>
2025-02-20 19:45:07 +0200

    added docstrings for preprocessing and updated deps versions

--- ppi_research/data_adapters/preprocessing_pipelines.py
+++ ppi_research/data_adapters/preprocessing_pipelines.py
@@ -1,11 +1,24 @@
+import typing
 from ppi_research.data_adapters import preprocessing
 
 
 def preprocess_multi_chain_sequences(
-    ligands: str, receptors: str
-) -> tuple[str, str]:
-    ligands = preprocessing.multi_chain_preprocessing(ligands)
-    receptors = preprocessing.multi_chain_preprocessing(receptors)
+    ligands: str,
+    receptors: str,
+    sep: str = ",",
+) -> typing.Tuple[str, str]:
+    """Preprocess a pair of multi-chain sequences.
+
+    Args:
+        ligands (str): The ligand sequence.
+        receptors (str): The receptor sequence.
+        sep (str, optional): The separator between the chains. Defaults to ",".
+
+    Returns:
+        typing.Tuple[str, str]: The processed ligand and receptor sequences.
+    """
+    ligands = preprocessing.multi_chain_preprocessing(ligands, sep)
+    receptors = preprocessing.multi_chain_preprocessing(receptors, sep)
     return ligands, receptors
 
 
@@ -14,7 +27,19 @@ def add_sep_tokens_between_chains(
     receptors: str,
     sep_token: str,
     merge_chains: bool = True,
-) -> tuple[str, str]:
+) -> typing.Tuple[str, str]:
+    """Add a separator token between chains.
+
+    Args:
+        ligands (str): The ligand sequence.
+        receptors (str): The receptor sequence.
+        sep_token (str): The separator token to insert.
+        merge_chains (bool, optional): Whether to merge chains.
+        Defaults to True.
+
+    Returns:
+        typing.Tuple[str, str]: The processed ligand and receptor sequences.
+    """
     ligands = preprocessing.insert_sep_token_between_chains(
         ligands,
         sep_token=sep_token,
@@ -30,6 +55,11 @@ def add_sep_tokens_between_chains(
 
 class SequencePairPreprocessingPipeline:
     def __init__(self, model_name: str):
+        """Initialize the preprocessing pipeline.
+
+        Args:
+            model_name (str): The name of the model.
+        """
         self.model_name = model_name.lower()
         if self.model_name == "esm":
             self._suffix = "<eos>"
@@ -47,10 +77,21 @@ class SequencePairPreprocessingPipeline:
         self,
         ligands: str,
         receptors: str,
-    ) -> tuple[str, str]:
+    ) -> typing.Tuple[str, str]:
+        """Preprocess a pair of sequences for ProtT5.
+
+        Args:
+            ligands (str): The ligand sequence.
+            receptors (str): The receptor sequence.
+
+        Returns:
+            typing.Tuple[str, str]: The processed ligand and
+            receptor sequences.
+        """
         ligands, receptors = preprocess_multi_chain_sequences(
             ligands, receptors
         )
+
         ligands = preprocessing.prott5_sequence_preprocessing(ligands)
         receptors = preprocessing.prott5_sequence_preprocessing(receptors)
 
@@ -66,7 +107,17 @@ class SequencePairPreprocessingPipeline:
         self,
         ligands: str,
         receptors: str,
-    ) -> tuple[str, str]:
+    ) -> typing.Tuple[str, str]:
+        """Preprocess a pair of sequences for Ankh.
+
+        Args:
+            ligands (str): The ligand sequence.
+            receptors (str): The receptor sequence.
+
+        Returns:
+            typing.Tuple[str, str]: The processed ligand and
+            receptor sequences.
+        """
         ligands, receptors = preprocess_multi_chain_sequences(
             ligands, receptors
         )
@@ -82,7 +133,17 @@ class SequencePairPreprocessingPipeline:
         self,
         ligands: str,
         receptors: str,
-    ) -> tuple[str, str]:
+    ) -> typing.Tuple[str, str]:
+        """Preprocess a pair of sequences for ESM.
+
+        Args:
+            ligands (str): The ligand sequence.
+            receptors (str): The receptor sequence.
+
+        Returns:
+            typing.Tuple[str, str]: The processed ligand and
+            receptor sequences.
+        """
         ligands, receptors = preprocess_multi_chain_sequences(
             ligands, receptors
         )
@@ -95,23 +156,57 @@ class SequencePairPreprocessingPipeline:
         receptors = ["<cls>"] + receptors
         return ligands, receptors
 
-    def preprocess(self, ligands: str, receptors: str) -> tuple[str, str]:
+    def preprocess(
+        self, ligands: str, receptors: str
+    ) -> typing.Tuple[str, str]:
         return self._preprocessing_function(ligands, receptors)
 
 
 class SequenceConcatPreprocessingPipeline(SequencePairPreprocessingPipeline):
     def __init__(self, model_name: str):
-        super().__init__(model_name)
+        """Initialize the concatenation pipeline.
+
+        Args:
+            model_name (str): The name of the model.
+        """
+        super(SequenceConcatPreprocessingPipeline, self).__init__(model_name)
 
     def _prott5_preprocessing(self, ligands: str, receptors: str) -> str:
+        """Preprocess a pair of sequences for ProtT5.
+
+        Args:
+            ligands (str): The ligand sequence.
+            receptors (str): The receptor sequence.
+
+        Returns:
+            str: The concatenated ligand and receptor sequences.
+        """
         ligands, receptors = super()._prott5_preprocessing(ligands, receptors)
         return ligands + receptors
 
     def _ankh_preprocessing(self, ligands: str, receptors: str) -> str:
+        """Preprocess a pair of sequences for Ankh.
+
+        Args:
+            ligands (str): The ligand sequence.
+            receptors (str): The receptor sequence.
+
+        Returns:
+            str: The concatenated ligand and receptor sequences.
+        """
         ligands, receptors = super()._ankh_preprocessing(ligands, receptors)
         return ligands + receptors
 
     def _esm_preprocessing(self, ligands: str, receptors: str) -> str:
+        """Preprocess a pair of sequences for ESM.
+
+        Args:
+            ligands (str): The ligand sequence.
+            receptors (str): The receptor sequence.
+
+        Returns:
+            str: The concatenated ligand and receptor sequences.
+        """
         ligands, receptors = super()._esm_preprocessing(ligands, receptors)
         # remove the cls token from the receptors
         return ligands + receptors[1:]
@@ -119,6 +214,11 @@ class SequenceConcatPreprocessingPipeline(SequencePairPreprocessingPipeline):
 
 class MultiChainPreprocessingPipeline:
     def __init__(self, model_name: str):
+        """Initialize the multi-chain preprocessing pipeline.
+
+        Args:
+            model_name (str): The name of the model.
+        """
         self.model_name = model_name.lower()
         if self.model_name == "prott5":
             self._preprocessing_function = self._prott5_preprocessing
@@ -133,7 +233,18 @@ class MultiChainPreprocessingPipeline:
         self,
         ligands: str,
         receptors: str,
-    ) -> tuple[list[str], list[str]]:
+    ) -> typing.Tuple[list[str], int, list[str], int]:
+        """Preprocess a pair of sequences for ProtT5.
+
+        Args:
+            ligands (str): The ligand sequence.
+            receptors (str): The receptor sequence.
+
+        Returns:
+            typing.Tuple[list[str], int, list[str], int]: The processed
+            ligand chains, number of ligand chains, receptor chains, and
+            number of receptor chains.
+        """
         ligands, receptors = preprocess_multi_chain_sequences(
             ligands, receptors
         )
@@ -154,7 +265,18 @@ class MultiChainPreprocessingPipeline:
         self,
         ligands: str,
         receptors: str,
-    ) -> tuple[list[str], list[str]]:
+    ) -> typing.Tuple[list[str], int, list[str], int]:
+        """Preprocess a pair of sequences for Ankh.
+
+        Args:
+            ligands (str): The ligand sequence.
+            receptors (str): The receptor sequence.
+
+        Returns:
+            typing.Tuple[list[str], int, list[str], int]: The processed
+            ligand chains, number of ligand chains, receptor chains, and
+            number of receptor chains.
+        """
         ligands, receptors = preprocess_multi_chain_sequences(
             ligands, receptors
         )
@@ -172,7 +294,18 @@ class MultiChainPreprocessingPipeline:
         self,
         ligands: str,
         receptors: str,
-    ) -> tuple[list[str], list[str]]:
+    ) -> typing.Tuple[list[str], int, list[str], int]:
+        """Preprocess a pair of sequences for ESM.
+
+        Args:
+            ligands (str): The ligand sequence.
+            receptors (str): The receptor sequence.
+
+        Returns:
+            typing.Tuple[list[str], int, list[str], int]: The processed
+            ligand chains, number of ligand chains, receptor chains, and
+            number of receptor chains.
+        """
         ligands, receptors = preprocess_multi_chain_sequences(
             ligands, receptors
         )
@@ -188,5 +321,16 @@ class MultiChainPreprocessingPipeline:
 
     def preprocess(
         self, ligands: str, receptors: str
-    ) -> tuple[list[str], list[str]]:
+    ) -> typing.Tuple[list[str], int, list[str], int]:
+        """Preprocess a pair of sequences.
+
+        Args:
+            ligands (str): The ligand sequence.
+            receptors (str): The receptor sequence.
+
+        Returns:
+            typing.Tuple[list[str], int, list[str], int]: The processed
+            ligand chains, number of ligand chains, receptor chains, and
+            number of receptor chains.
+        """
         return self._preprocessing_function(ligands, receptors)
